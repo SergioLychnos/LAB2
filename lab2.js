137694ce25958a5be4a6110a9e61c1cc4b239b3a
VibeDasTribes <mrbungle_fever@hotmail.com>
2023-06-22 10:18:37 -0400

    adding hello to the h1

--- lab2.js
+++ lab2.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));     
 
 app.get("/", (req, res) =>{                                               
-    res.send(`<h1>WEB222 LAB2<h1>`);
+    res.send(`<h1>Hello WEB222 LAB2<h1>`);
 });
 
 
